298a830b7a1631ddbf203db369199ca96b740826
-1 <-1@HOME-PC>
2024-05-09 00:01:31 +0300

    Добавьте файлы проекта.

---
+++ ASP_LR10/Program.cs
@@ -0,0 +1,19 @@
+using ASP_LR10;
+
+namespace ASP_LR10
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
